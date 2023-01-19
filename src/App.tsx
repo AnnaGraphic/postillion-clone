4235fe51214d61c23198b055f372561e7108e807
annar <p4nd4@posteo.de>
2023-01-19 15:00:02 +0100

    use interfaces | fetch data from db

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Title from './components/Title'
+import { QuizData } from '../interfaces'
+
 
 const App = () => {
+  const [quiz, setQuiz] = useState <QuizData | null> ()
+
+
+const fetchData = async () => {
+  try {
+    const response = await fetch ('http://localhost:8000/quiz-item')
+    const json = await response.json()
+    console.log(json)
+    setQuiz(json)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+useEffect(()=> {
+  fetchData()
+}, [])
+
   return (
     <div>
-<Title/>
+ < Title title={quiz?.title} subtitle={quiz?.subtitle} />
     </div>
   );
 }
