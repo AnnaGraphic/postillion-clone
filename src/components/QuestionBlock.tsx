c533324175f1a1fb4c310604650c70fd49b33047
annar <p4nd4@posteo.de>
2023-01-30 16:04:24 +0100

    add images and texts to Component

--- src/components/QuestionBlock.tsx
+++ src/components/QuestionBlock.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Question } from '../../interfaces'
 
-const QuestionBlock = () => {
+const QuestionBlock = ({ 
+    question 
+} : {
+    question : Question
+}) => {
+const handleClick = () => {console.log('click') } 
+
     return (
-        <div></div>
+        <div>
+            <button
+            className='question-block'
+            onClick={handleClick}
+            >
+            <img src={question.image} alt={question.alt}/>
+            <h3>{question.text}</h3>
+            <p>
+                <a href={question.image}>question.credit</a>
+               // <a href=""></a>
+            </p>
+            </button>
+        </div>
     )
 }
 export default QuestionBlock
